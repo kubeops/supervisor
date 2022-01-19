50b855a8b2a01ed6c80dd1f6a51e98ada1c1447b
Pulak Kanti Bhowmick <pulak@appscode.com>
2022-01-19 12:54:20 +0600

    Add maintenance window yaml
    Signed-off-by: Pulak Kanti Bhowmick <pulak@appscode.com>

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	"flag"
+	supervisorv1alpha1 "kubeops.dev/supervisor/apis/supervisor/v1alpha1"
 	"os"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
@@ -31,7 +32,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	supervisorv1alpha1install "kubeops.dev/supervisor/apis/supervisor/install"
 	supervisorcontrollers "kubeops.dev/supervisor/controllers/supervisor"
 	//+kubebuilder:scaffold:imports
 )
@@ -42,10 +42,9 @@ var (
 )
 
 func init() {
-	supervisorv1alpha1install.Install(scheme)
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	//utilruntime.Must(supervisorv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(supervisorv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
