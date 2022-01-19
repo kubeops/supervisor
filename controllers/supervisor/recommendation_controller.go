2fb65ac3d57b9a11d7ac900ca327e5eebd27b5d9
Pulak Kanti Bhowmick <pulak@appscode.com>
2022-01-19 18:15:40 +0600

    Modify shared func
    Signed-off-by: Pulak Kanti Bhowmick <pulak@appscode.com>

--- controllers/supervisor/recommendation_controller.go
+++ controllers/supervisor/recommendation_controller.go
@@ -46,10 +46,6 @@ type RecommendationReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Recommendation object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
@@ -77,7 +73,7 @@ func (r *RecommendationReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 	rcmd = obj.(*supervisorv1alpha1.Recommendation)
 
 	if rcmd.Status.ApprovalStatus == supervisorv1alpha1.ApprovalApproved {
-		exeObj, err := shared.GetExecutableObject(rcmd.Spec.Operation)
+		exeObj, err := shared.GetOpsRequestObject(rcmd.Spec.Operation)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
@@ -88,7 +84,7 @@ func (r *RecommendationReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 	return ctrl.Result{}, nil
 }
 
-func (r *RecommendationReconciler) ExecuteOperation(e apis.Executable) error {
+func (r *RecommendationReconciler) ExecuteOperation(e apis.OpsRequest) error {
 	return e.Execute(r.Client)
 }
 
--- pkg/shared/shared.go
+++ pkg/shared/shared.go
@@ -1,16 +1,24 @@
 package shared
 
 import (
+	"encoding/json"
+	"fmt"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	opsapi "kubedb.dev/apimachinery/apis/ops/v1alpha1"
 	"kubeops.dev/supervisor/apis"
 	mongodb_ops "kubeops.dev/supervisor/pkg/mongodb-ops"
 )
 
-func GetExecutableObject(obj runtime.RawExtension) (apis.Executable, error) {
-	// do it now only for MongoDB
-	mgOps, err := mongodb_ops.NewMongoDBOpsRequest(obj)
-	if err != nil {
+func GetOpsRequestObject(obj runtime.RawExtension) (apis.OpsRequest, error) {
+	unObj := &unstructured.Unstructured{}
+	if err := json.Unmarshal(obj.Raw, unObj); err != nil {
 		return nil, err
 	}
-	return mgOps, nil
+	gvk := unObj.GetObjectKind().GroupVersionKind()
+
+	if gvk.Group == opsapi.SchemeGroupVersion.Group && gvk.Kind == opsapi.ResourceKindMongoDBOpsRequest {
+		return mongodb_ops.NewMongoDBOpsRequest(obj)
+	}
+	return nil, fmt.Errorf("invalid operation, Group: %v Kind: %v is not supported", gvk.Group, gvk.Kind)
 }
