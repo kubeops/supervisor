2508d4020a9d3acb5a25c9c34e5b9ed8e6cf57cb
Pulak Kanti Bhowmick <pulak@appscode.com>
2022-01-21 11:21:15 +0600

    Implement requeue logic
    Signed-off-by: Pulak Kanti Bhowmick <pulak@appscode.com>

--- apis/supervisor/v1alpha1/constants.go
+++ apis/supervisor/v1alpha1/constants.go
@@ -3,3 +3,10 @@ package v1alpha1
 const (
 	DefaultMaintenanceWindowKey = "supervisor.appscode.com/is-default-maintenance-window"
 )
+
+// List of Condition reasons
+
+const (
+	RecommendationSuccessfullyCreated = "RecommendationSuccessfullyCreated"
+	OpsRequestSuccessfullyCreated     = "OpsRequestSuccessfullyCreated"
+)
