62045414de95e2709125d40080f0ca2d76a27d29
Pulak Kanti Bhowmick <pulak@appscode.com>
2022-01-26 12:55:56 +0600

    Implement QueuePerNamespace Parallelism feature
    Signed-off-by: Pulak Kanti Bhowmick <pulak@appscode.com>

--- pkg/parallelism/parallelism.go
+++ pkg/parallelism/parallelism.go
@@ -3,6 +3,7 @@ package parallelism
 import (
 	"context"
 
+	kmapi "kmodules.xyz/client-go/api/v1"
 	supervisorv1alpha1 "kubeops.dev/supervisor/apis/supervisor/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -22,5 +23,24 @@ func NewParallelRunner(ctx context.Context, kc client.Client, rcmd *supervisorv1
 }
 
 func (r *ParallelRunner) MaintainParallelism() (bool, error) {
+	if r.rcmd.Status.Parallelism == supervisorv1alpha1.QueuePerNamespace {
+		return r.isMaintainingQueuePerNamespace()
+	}
 	return false, nil
+}
+
+func (r *ParallelRunner) isMaintainingQueuePerNamespace() (bool, error) {
+	rcmdList := &supervisorv1alpha1.RecommendationList{}
+
+	if err := r.kc.List(r.ctx, rcmdList, client.InNamespace(r.rcmd.Namespace)); err != nil {
+		return false, err
+	}
+
+	for _, rc := range rcmdList.Items {
+		if kmapi.IsConditionTrue(rc.Status.Conditions, supervisorv1alpha1.SuccessfullyCreatedOperation) &&
+			!kmapi.HasCondition(rc.Status.Conditions, supervisorv1alpha1.SuccessfullyExecutedOperation) {
+			return false, nil
+		}
+	}
+	return true, nil
 }
