dba0c6d5af0dfbf33b2662d246b927cf0ceeb450
Pulak Kanti Bhowmick <pulak@appscode.com>
2022-01-27 12:31:56 +0600

    Implement QueuePerTarget & QueuePerTargetAndNamespac feature
    Signed-off-by: Pulak Kanti Bhowmick <pulak@appscode.com>

--- pkg/parallelism/parallelism.go
+++ pkg/parallelism/parallelism.go
@@ -2,7 +2,9 @@ package parallelism
 
 import (
 	"context"
+	"errors"
 
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	kmapi "kmodules.xyz/client-go/api/v1"
 	supervisorv1alpha1 "kubeops.dev/supervisor/apis/supervisor/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -25,8 +27,12 @@ func NewParallelRunner(ctx context.Context, kc client.Client, rcmd *supervisorv1
 func (r *ParallelRunner) MaintainParallelism() (bool, error) {
 	if r.rcmd.Status.Parallelism == supervisorv1alpha1.QueuePerNamespace {
 		return r.isMaintainingQueuePerNamespace()
+	} else if r.rcmd.Status.Parallelism == supervisorv1alpha1.QueuePerTarget {
+		return r.isMaintainingQueuePerTarget()
+	} else if r.rcmd.Status.Parallelism == supervisorv1alpha1.QueuePerTargetAndNamespace {
+		return r.isMaintainingQueuePerTargetAndNamespace()
 	}
-	return false, nil
+	return false, errors.New("invalid parallelism type")
 }
 
 func (r *ParallelRunner) isMaintainingQueuePerNamespace() (bool, error) {
@@ -37,6 +43,63 @@ func (r *ParallelRunner) isMaintainingQueuePerNamespace() (bool, error) {
 	}
 
 	for _, rc := range rcmdList.Items {
+		if kmapi.IsConditionTrue(rc.Status.Conditions, supervisorv1alpha1.SuccessfullyCreatedOperation) &&
+			!kmapi.HasCondition(rc.Status.Conditions, supervisorv1alpha1.SuccessfullyExecutedOperation) {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+func (r *ParallelRunner) isMaintainingQueuePerTargetAndNamespace() (bool, error) {
+	gv, err := schema.ParseGroupVersion(*r.rcmd.Spec.Target.APIGroup)
+	if err != nil {
+		return false, err
+	}
+	reqGK := schema.GroupKind{
+		Group: gv.Group,
+		Kind:  r.rcmd.Spec.Target.Kind,
+	}
+
+	rcmdList := &supervisorv1alpha1.RecommendationList{}
+	if err := r.kc.List(r.ctx, rcmdList, client.InNamespace(r.rcmd.Namespace)); err != nil {
+		return false, err
+	}
+	return isMaintainingQueuePerGK(reqGK, rcmdList)
+}
+
+func (r *ParallelRunner) isMaintainingQueuePerTarget() (bool, error) {
+	gv, err := schema.ParseGroupVersion(*r.rcmd.Spec.Target.APIGroup)
+	if err != nil {
+		return false, err
+	}
+	reqGK := schema.GroupKind{
+		Group: gv.Group,
+		Kind:  r.rcmd.Spec.Target.Kind,
+	}
+
+	rcmdList := &supervisorv1alpha1.RecommendationList{}
+	if err := r.kc.List(r.ctx, rcmdList); err != nil {
+		return false, err
+	}
+	return isMaintainingQueuePerGK(reqGK, rcmdList)
+}
+
+func isMaintainingQueuePerGK(reqGK schema.GroupKind, rcList *supervisorv1alpha1.RecommendationList) (bool, error) {
+	for _, rc := range rcList.Items {
+		gv, err := schema.ParseGroupVersion(*rc.Spec.Target.APIGroup)
+		if err != nil {
+			return false, err
+		}
+
+		gk := schema.GroupKind{
+			Group: gv.Group,
+			Kind:  rc.Spec.Target.Kind,
+		}
+		if reqGK != gk {
+			continue
+		}
+
 		if kmapi.IsConditionTrue(rc.Status.Conditions, supervisorv1alpha1.SuccessfullyCreatedOperation) &&
 			!kmapi.HasCondition(rc.Status.Conditions, supervisorv1alpha1.SuccessfullyExecutedOperation) {
 			return false, nil
