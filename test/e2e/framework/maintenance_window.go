0febd571d60c6a116d8d1ab4e6cfbcd5b422eedd
Pulak Kanti Bhowmick <pulak@appscode.com>
2022-02-01 17:22:20 +0600

    Add e2e test for given maintenance window
    Signed-off-by: Pulak Kanti Bhowmick <pulak@appscode.com>

--- test/e2e/framework/maintenance_window.go
+++ test/e2e/framework/maintenance_window.go
@@ -3,6 +3,10 @@ package framework
 import (
 	"time"
 
+	kmapi "kmodules.xyz/client-go/api/v1"
+
+	"gomodules.xyz/x/crypto/rand"
+
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -44,6 +48,38 @@ func (f *Framework) CreateDefaultMaintenanceWindow() error {
 	})
 }
 
+func (f *Framework) CreateMaintenanceWindow(days map[api.DayOfWeek][]api.TimeWindow, dates []api.DateWindow) (*api.MaintenanceWindow, error) {
+	mw := &api.MaintenanceWindow{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      rand.WithUniqSuffix("supervisor-mw"),
+			Namespace: f.namespace,
+		},
+		Spec: api.MaintenanceWindowSpec{
+			Dates: dates,
+			Days:  days,
+		},
+	}
+	err := f.kc.Create(f.ctx, mw)
+	if err != nil {
+		return nil, err
+	}
+
+	err = wait.PollImmediate(time.Second, time.Minute, func() (bool, error) {
+		mwObj := &api.MaintenanceWindow{}
+		key := client.ObjectKey{Namespace: mw.Namespace, Name: mw.Name}
+
+		if err := f.kc.Get(f.ctx, key, mwObj); err != nil {
+			return false, client.IgnoreNotFound(err)
+		}
+
+		return true, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return mw, nil
+}
+
 func (f *Framework) defaultMaintenanceWindowName() string {
 	return f.name
 }
@@ -57,6 +93,64 @@ func (f *Framework) DeleteDefaultMaintenanceWindow() error {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      f.defaultMaintenanceWindowName(),
 			Namespace: f.defaultMaintenanceWindowNamespace(),
+		},
+	}
+
+	return f.kc.Delete(f.ctx, mw)
+}
+
+func (f *Framework) GetAllDayOfWeekTimeWindow() map[api.DayOfWeek][]api.TimeWindow {
+	return map[api.DayOfWeek][]api.TimeWindow{
+		api.Monday: {
+			{
+				Start: kmapi.Date(0, 0, 0),
+				End:   kmapi.Date(23, 59, 59),
+			},
+		},
+		api.Tuesday: {
+			{
+				Start: kmapi.Date(0, 0, 0),
+				End:   kmapi.Date(23, 59, 59),
+			},
+		},
+		api.Wednesday: {
+			{
+				Start: kmapi.Date(0, 0, 0),
+				End:   kmapi.Date(23, 59, 59),
+			},
+		},
+		api.Thursday: {
+			{
+				Start: kmapi.Date(0, 0, 0),
+				End:   kmapi.Date(23, 59, 59),
+			},
+		},
+		api.Friday: {
+			{
+				Start: kmapi.Date(0, 0, 0),
+				End:   kmapi.Date(23, 59, 59),
+			},
+		},
+		api.Saturday: {
+			{
+				Start: kmapi.Date(0, 0, 0),
+				End:   kmapi.Date(23, 59, 59),
+			},
+		},
+		api.Sunday: {
+			{
+				Start: kmapi.Date(0, 0, 0),
+				End:   kmapi.Date(23, 59, 59),
+			},
+		},
+	}
+}
+
+func (f *Framework) DeleteMaintenanceWindow(key client.ObjectKey) error {
+	mw := &api.MaintenanceWindow{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      key.Name,
+			Namespace: key.Namespace,
 		},
 	}
 
