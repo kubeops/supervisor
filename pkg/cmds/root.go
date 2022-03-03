de6720d85dc748a97e962bfd5c66e0c99642b528
Pulak Kanti Bhowmick <pulak@appscode.com>
2022-03-03 11:17:53 +0600

    Add RequireExplicitApproval & Outdated field in Recommendation API
    Signed-off-by: Pulak Kanti Bhowmick <pulak@appscode.com>

--- pkg/cmds/root.go
+++ pkg/cmds/root.go
@@ -25,7 +25,7 @@ import (
 )
 
 func NewRootCmd() *cobra.Command {
-	var rootCmd = &cobra.Command{
+	rootCmd := &cobra.Command{
 		Use:               "supervisor [command]",
 		Short:             `Supervisor Operator by AppsCode`,
 		DisableAutoGenTag: true,
