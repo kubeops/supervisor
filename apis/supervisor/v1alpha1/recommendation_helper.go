e11ff308ff002e79e25f74f3f3063968edb6643e
Pulak Kanti Bhowmick <pulak@appscode.com>
2022-08-12 10:47:51 +0600

    Add helper methods
    Signed-off-by: Pulak Kanti Bhowmick <pulak@appscode.com>

---
+++ apis/supervisor/v1alpha1/recommendation_helper.go
@@ -0,0 +1,38 @@
+/*
+Copyright AppsCode Inc. and Contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+func (r *Recommendation) IsAwaitingOrProgressingRecommendation() bool {
+	return r.IsAwaitingRecommendation() || r.IsProgressingRecommendation()
+}
+
+func (r *Recommendation) IsAwaitingRecommendation() bool {
+	switch r.Status.Phase {
+	case "":
+		fallthrough
+	case Pending:
+		fallthrough
+	case Waiting:
+		return true
+	default:
+		return false
+	}
+}
+
+func (r *Recommendation) IsProgressingRecommendation() bool {
+	return r.Status.Phase == InProgress
+}
